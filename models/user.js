33ef343b1f6972824f7f41996d8da3b3033244fc
Manuel Oyanedel Cid <cethem3@gmail.com>
2020-09-29 21:43:49 -0300

    Commit Tarea 1, modelos tweet y user

--- models/user.js
+++ models/user.js
@@ -14,9 +14,10 @@ const userSchema = new mongoose.Schema(
             trim: true,
             required: true,
         },
-        hashed_password: {
+        password: {
             type: String,
             required: true,
+            trim: true,
         },
         birthdate: {
             type: Date,
@@ -57,35 +58,5 @@ const userSchema = new mongoose.Schema(
         timestamps: true, 
     },
 )
-
-// control virtuals fields
-userSchema
-    .virtual("password")
-    .set((password) => {
-        this._password = password
-        this.salt = uuidv1()
-        this.hashed_password = this.encryptPassword(password)
-    })
-    .get(() => {
-        return this._password
-    })
-
-// schemas methods
-userSchema.methods = {
-    authenticate: (plaintext) => {
-        return this.encryptPassword(plaintext) === this.hashed_password
-    },
-    encryptPassword : (password) => {
-        if(!password) return ""
-        try {
-            return crypto
-                .createHmac('sha1', this.salt)
-                .update(password)
-                .digest('hex')
-        } catch(e) {
-            return ""
-        }
-    }
-}
 
 module.exports = mongoose.model("User", userSchema)
